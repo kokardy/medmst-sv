2e35492c6a854e22c3c6a938e69192a8bdf0c181
kokardy <2720900+kokardy@users.noreply.github.com>
2017-12-27 10:21:37 +0900

    register.py insertsql追加

--- asset/register.py
+++ asset/register.py
@@ -4,60 +4,86 @@ import psycopg2 as pg
 import csv
 import os, re
 import os.path 
-import 
 
-SAVE_DIR = "/bootstrap/save"
-CURRENT_DIR = "./"
+if "MEDMST_SAVE" in os.environ:
+    SAVE_DIR = os.environ["MEDMST_SAVE"]
+else:
+    SAVE_DIR = "/bootstrap/save"
 
-def getConnection():
+PARAM = dict(
+            host = os.environ["MEDMST_PG_HOST"],
+            port = os.environ["MEDMST_PG_PORT"],
+            user = os.environ["MEDMST_PG_USER"],
+            password = os.environ["MEDMST_PG_PASSWORD"],
+)
+
+def connection():
+    return _getConnection(PARAM)
+
+def _connection(param):
     return pg.connect(**param)
 
-def get_files(path, hot="hot", y="y"):
-    result = dict(hot=dict())
-    for path, dirs, filenames in os.walk(SAVE_DIR):
+def get_files(save_dir=SAVE_DIR):
+    hot = "hot"
+    y = "y"
+    result = dict(hot=[], y=[])
+    for path, dirs, filenames in os.walk(save_dir):
         for filename in filenames:
             fullpath = os.path.join(path, filename)
             if os.path.basename(path) == hot:
                 r = result[hot]
                 if re.match("^MEDIS\d{8}.TXT$", filename):
-                    r["main"] = fullpath
+                    r.append(fullpath)
                 elif re.match("^\d{8}.txt", filename):
-                    r["extra"] = r.get("extra", []).append(fullpath)
+                    r.append(fullpath)
                 elif re.match("^h\d{8}del.txt", filename):
-                    r["delete"] = fullpath
+                    r.append(fullpath)
             if os.path.basename(path) == y:
                 if filename == "y.csv":
-                    result[y] = fullpath
+                    result[y] = [fullpath]
+
+    return result
+
+def create(con):
+    filepath = os.path.join(SAVE_DIR, "medis_def.txt")
+    _sql_from_file(filepath)
+    cur = con.cursor()
+    cur.execute(sql)
+    filepath = os.path.join(SAVE_DIR, "y_def.txt")
+    _sql_from_file(filepath)
+    cur = con.cursor()
+    cur.execute(sql)
+
+def _sql_from_file(filepath):
+    with codecs.open(filepath, "r", "utf8") as f:
+        lines = [line for line in f]
+        sql = "\n".join(lines)
+    return sql
+
+def insert(con, infiles):
+    infiles = get_files()
+    
+    insert_list = [(os.path.join(SAVE_DIR, "{0}_insert.txt").format(table),
+        infiles[table], skip)
+            for (table, skip) in [("medis", True), ("y", False)]]
+    for (sql_file, insert_data, skip) in insert_list:
+        _insert(con, sql_file, insert_data, skip)
+
+def _insert(con, sql_file, insert_files, line1skip):
+    sql = _sql_from_file(sql_file)
+    for insert_file in insert_files:
+        with codecs.open(insert_file, "r", "utf-8") as f:
+            r = csv.reader(f)
+            if line1skip:
+                r.next()
+            cur = con.cursor()
+            cur.executemany(sql, r)
 
 
 def main():
     infiles = get_files(SAVE_DIR)
     print(infiles)
-
-
-def create(con, tables=["hot", "y"]):
-    if "hot" in tables:
-        createHOT(con)
-    if "y" in tables:
-        createY(con)
-
-def createHOT(con):
-    filepath = os.path.join(CURRENT_DIR, "./medis_def.txt")
-    f = codecs.open(filepath, "r", "utf8")
-    lines = [line for line in f]
-    sql = "\n".join(lines)
-    cur = con.cursor()
-    cur.execute(sql)
-
-def createY(con):
-    filepath = os.path.join(CURRENT_DIR, "./y_def.txt")
-    f = codecs.open(filepath, "r", "utf8")
-    lines = [line for line in f]
-    sql = "\n".join(lines)
-    cur = con.cursor()
-    cur.execute(sql)
-
-
+    connection()
 
 if __name__ == '__main__':
     main()
