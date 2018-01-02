16c2655c96c6ae002a38781c79a0b19c939bd979
kokardy <2720900+kokardy@users.noreply.github.com>
2018-01-03 01:54:34 +0900

    implementing server.go

---
+++ asset/dbstruct.go
@@ -0,0 +1,5 @@
+package main
+
+type Medis struct {
+	Name string
+}
--- asset/server.go
+++ asset/server.go
@@ -2,14 +2,45 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func connect_param() (param string) {
+
+	return
+}
+
+func medis_handler(w http.ResponseWriter, r *http.Request) {
+	barcode := r.FormValue("barcode")
+	gs1 := GS1(barcode)
+	if !gs1.CheckDigitOK() {
+		fmt.Fprintf(w, "wrong barcode: checkdigit error")
+		fmt.Fprintf(w, barcode)
+	}
+	jan := gs1.ToJAN()
+	param := connect_param()
+	db, err := sqlx.Connect("postgresql", param)
+	if err != nil {
+		fmt.Fprintf(w, "an ERROR occured in database connecting")
+	}
+
+	sql := `SELECT "薬品名" FROM "medis" WHERE "JANコード";`
+
+	medis := Medis{}
+	db.Get(&medis, sql, string(jan))
+
+	fmt.Fprintf(w, "Hello, World")
+	fmt.Fprintf(w, barcode)
+}
+func y_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
 }
 
 func main() {
-	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+	http.HandleFunc("/medis", medis_handler)
+	http.HandleFunc("/y", y_handler)
 	http.ListenAndServe(":8080", nil)
 }
