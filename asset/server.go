46497132e1ac04665b400a496cfccd1c0b8227b1
Kota Kokado <kouta-k@mbm.nifty.com>
2018-01-04 17:14:33 +0900

    バーコードから薬品検索するURLの実装

--- asset/server.go
+++ asset/server.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 
+	"encoding/json"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"net/http"
 )
+
+type Medis struct {
+	Name  string
+	Total string
+	Unit  string
+}
 
 func connect_param() (param string) {
 
@@ -15,25 +22,46 @@ func connect_param() (param string) {
 
 func medis_handler(w http.ResponseWriter, r *http.Request) {
 	barcode := r.FormValue("barcode")
-	gs1 := GS1(barcode)
+	gs1 := GS1(barcode) //GS1として読んでみる
+	var jan JAN
+	//validate check digit
 	if !gs1.CheckDigitOK() {
-		fmt.Fprintf(w, "wrong barcode: checkdigit error")
-		fmt.Fprintf(w, barcode)
+		jan = JAN(barcode) //ダメならJANとして読んでみる
+		if !jan.CheckDigitOK() {
+			fmt.Fprintf(w, "wrong barcode: checkdigit error")
+			fmt.Fprintf(w, barcode)
+			return
+		}
+	} else { //GS1ならjanに変換する
+		jan = gs1.ToJAN()
 	}
-	jan := gs1.ToJAN()
 	param := connect_param()
 	db, err := sqlx.Connect("postgresql", param)
+	//DB connection check
 	if err != nil {
 		fmt.Fprintf(w, "an ERROR occured in database connecting")
+		return
 	}
 
-	sql := `SELECT "薬品名" FROM "medis" WHERE "JANコード";`
+	sql := `SELECT 
+				"販売名",
+				"包装総量数",
+				"包装総量単位" 
+			FROM "medis"
+			WHERE "ＪＡＮコード" = $1;`
 
 	medis := Medis{}
-	db.Get(&medis, sql, string(jan))
+	err = db.Get(&medis, sql, string(jan))
+	//sql execute error
+	if err != nil {
+		fmt.Fprintf(w, "an ERROR occured in executing SQL")
+		fmt.Fprintf(w, fmt.Sprintf("SQL:%s", sql))
+		return
+	}
 
-	fmt.Fprintf(w, "Hello, World")
-	fmt.Fprintf(w, barcode)
+	w.Header().Set("Content-Type", "application/json")
+	json.Marshal(w, medis)
+	return
 }
 func y_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
