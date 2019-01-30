815d0882a6e521bfeca271a33e9b63b5f8b3262e
Kota Kokado <kouta-k@mbm.nifty.com>
2019-01-30 15:59:37 +0900

    medmstのoption変更

--- asset/init.sh
+++ asset/init.sh
@@ -2,9 +2,7 @@ go get github.com/kokardy/medmst
 go get github.com/lib/pq
 go get github.com/jmoiron/sqlx
 cd /bootstrap
-DATE=`date +%Y%m%d`
-/go/bin/medmst -d $DATE -p $http_proxy -f
-mv $DATE save
+/go/bin/medmst -p $http_proxy -f
 cd /bootstrap/save/hot
 jlha xif *.lzh
 cd /bootstrap/save/y
