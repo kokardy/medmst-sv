ee1c7aa0810c1c844648343604dbad508bc227a2
Kota Kokado <kouta-k@mbm.nifty.com>
2019-02-01 14:44:21 +0900

    medmst option removed

--- asset/init.sh
+++ asset/init.sh
@@ -2,7 +2,7 @@ go get github.com/kokardy/medmst
 go get github.com/lib/pq
 go get github.com/jmoiron/sqlx
 cd /bootstrap
-/go/bin/medmst -p $http_proxy -f
+/go/bin/medmst -f
 cd /bootstrap/save/hot
 jlha xif *.lzh
 cd /bootstrap/save/y
