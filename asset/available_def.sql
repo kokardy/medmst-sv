4ee3fce7d421c67ac94f6d3395dbc0f67bb6f486
kokardy <2720900+kokardy@users.noreply.github.com>
2019-02-15 18:05:50 +0900

    採用ステータステーブル

--- asset/available_def.sql
+++ asset/available_def.sql
@@ -1,30 +1,31 @@
 CREATE TABLE "status" (
-    "id" primary key,
+    "no" integer primary key,
     "name" character varying(10)
 );
+
 INSERT INTO "status" VALUES(
-    "id" = 0,
-    "name" = "不採用"
+    0,
+    '不採用'
 );
 INSERT INTO "status" VALUES(
-    "id" = 1,
-    "name" = "採用薬"
+    1,
+    '採用薬'
 );
 INSERT INTO "status" VALUES(
-    "id" = 2,
-    "name" = "院外専用"
+    2,
+    '院外専用'
 );
 INSERT INTO "status" VALUES(
-    "id" = 3,
-    "name" = "院内専用"
+    4,
+    '院内専用'
 );
 
 CREATE TABLE "yj" (
     "yjcode" character varying(12) primary key,
-    "status_id" integer REFERENCIES status(id)
+    "status_no" integer REFERENCES status (no)
 );
 
 CREATE TABLE "hot" (
     "hot11" character varying(11) primary key,
-    "status_id" integer REFERENCIES status(id)
+    "status_no" integer REFERENCES status (no)
 );
