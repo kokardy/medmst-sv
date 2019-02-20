09ad26a5bdecc99ba6dda054adad8aaafe3b698e
kokardy <2720900+kokardy@users.noreply.github.com>
2019-02-21 00:18:06 +0900

    HandleMedis works

--- asset/barcode.go
+++ asset/barcode.go
@@ -5,14 +5,17 @@ import (
 )
 
 const (
-	GS1TYPE_ITEM = "01"
+	//GS1TYPEITEM is GS1コードのアイテム接頭番号
+	GS1TYPEITEM = "01"
 )
 
+//Code has checkdisit
 type Code interface {
 	CheckDigit() (string, string)
 	CheckDigitOk() bool
 }
 
+//CalcCheckDigit calcs checkdigit
 func CalcCheckDigit(code string, offset, length int) (string, string) {
 	count := 0
 	for i := offset; i < length-1; i++ {
@@ -29,12 +32,15 @@ func CalcCheckDigit(code string, offset, length int) (string, string) {
 	return strconv.Itoa(digit), string(code[length-1])
 }
 
+//JAN is JAN CODE
 type JAN string
 
+//CheckDigit calc checkdigit
 func (j JAN) CheckDigit() (string, string) {
 	return CalcCheckDigit(string(j), 0, 13)
 }
 
+//CheckDigitOK validates checkdigit
 func (j JAN) CheckDigitOK() bool {
 	if d1, d2 := j.CheckDigit(); d1 == d2 {
 		return true
@@ -42,11 +48,15 @@ func (j JAN) CheckDigitOK() bool {
 	return false
 }
 
+//GS1 is GS1 code
 type GS1 string
 
+//CheckDigit calc checkdigit
 func (g GS1) CheckDigit() (string, string) {
 	return CalcCheckDigit(string(g), 2, 16)
 }
+
+//CheckDigitOK validates checkdigit
 func (g GS1) CheckDigitOK() bool {
 	if d1, d2 := g.CheckDigit(); d1 == d2 {
 		return true
@@ -54,8 +64,9 @@ func (g GS1) CheckDigitOK() bool {
 	return false
 }
 
+//ToJAN converts GS1 to JAN
 func (g GS1) ToJAN() JAN {
-	pre_jan := JAN(string(g[3:16]))
-	check_digit, _ := pre_jan.CheckDigit()
-	return JAN(string(g[3:15]) + check_digit)
+	preJan := JAN(string(g[3:16]))
+	checkDigit, _ := preJan.CheckDigit()
+	return JAN(string(g[3:15]) + checkDigit)
 }
