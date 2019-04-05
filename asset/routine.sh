28a3833ecc1a336cd9b5b2814a76f92e43c08ecf
kokardy <2720900+kokardy@users.noreply.github.com>
2019-04-05 14:38:00 +0900

    routine.sh

--- asset/routine.sh
+++ asset/routine.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+day=`date '+%Y%m%d'`
+mv /bootstrap/save /bootstrap/${day}backup
+cd /bootstrap
+/go/bin/medmst -f
+cd /bootstrap/save/hot
+jlha xif *.lzh
+cd /bootstrap/save/y
+unzip -jo y.zip
+cd /bootstrap
 cd /asset
 python3 register.py -DI
