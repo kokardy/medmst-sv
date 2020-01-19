4bf6d3016875e01b8befe44854fecaab741897cf
kokardy <2720900+kokardy@users.noreply.github.com>
2020-01-19 14:16:19 +0900

    cron実行日時変更

--- asset/cron.sh
+++ asset/cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 printenv | awk '{print "export " $1}' > /root/env.sh
-echo "00 7,18 * * *  /asset/backup.sh >> /var/log/backup.log 2>&1" | crontab -
+echo "0 7,18 * * *  /asset/backup.sh >> /var/log/backup.log 2>&1" | crontab -
 /usr/sbin/cron -f -L 15
