0ce2b072b70e09d430d21a5c3f657f885e510993
Kota Kokado <kouta-k@mbm.nifty.com>
2020-03-30 15:57:50 +0900

    backup.sh works

--- asset/backup.sh
+++ asset/backup.sh
@@ -5,7 +5,7 @@
 date
 #echo "${PG_HOST}:${PG_PORT}:${PG_DATABASE}:${PG_USER}:${PG_PASSWORD}" 
 #echo "${PG_HOST}:${PG_PORT}:${PG_DATABASE}:${PG_USER}:${PG_PASSWORD}" > ~/.pgpass
-#echo "*:*:*:*:${PG_PASSWORD}" > ~/.pgpass
+echo "*:*:*:*:${PG_PASSWORD}" > ~/.pgpass
 #sudo su postgres
 #cat ~/.pgpass
 
