b80d2ec24ecbf5287718297e6d42ec55522a8fb3
kokardy <mgc1028@gmail.com>
2020-07-10 20:43:01 +0900

    custom_yj works correctly

--- asset/m.sql
+++ asset/m.sql
@@ -1,3 +1,5 @@
 --select count(*) from available_view;
 --select count(*) from medis;
-select * from available_view where "個別医薬品コード" like '3961008F1020';
+--select * from available_view where "個別医薬品コード" like '3961008F1020';
+select * from yj;
+select "個別医薬品コード"='', "個別医薬品コード", custom_yj, yj_status  from available_view where "HOT11" like '18200030401';
